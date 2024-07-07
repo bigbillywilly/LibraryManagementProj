2336e4032b16fc447e3790c6bf251174235e3b60
William Ngo <89047738+bigbillywilly@users.noreply.github.com>
2024-07-07 17:11:10 -0600

    Update Admin.java

--- Admin.java
+++ Admin.java
@@ -1,27 +1,48 @@
 package libraryManageProj;
 
-public class Admin extends User{
-	
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Date;
+
+public class Admin extends User {
+
 	private String adminID;
-	
+
 	public Admin(String name, String username, String email, String adminID) {
 		super(name, username, email);
 		this.adminID = adminID;
 	}
 
-	public void setAdminID(String adminID) {
-		this.adminID = adminID;
-	}
-	
 	public String getAdminID() {
 		return this.adminID;
 	}
-	
+
 	public void addBook(Catalog catalog, Book book) {
-		
+		catalog.addBook(book);
+	}
+
+	public void removeBook(Catalog catalog, Book book) {
+		catalog.removeBook(book);
 	}
 	
-	public void removeBook(Catalog catalog, Book book) {
-		
+	public void addBooks(Catalog catalog, Collection<? extends Book> books) {
+		catalog.addBooks(books);
+	}
+	
+	public void removeBooks(Catalog catalog, Collection<? extends Book> books) {
+		catalog.removeBooks(books);
+	}
+
+	public Date checkOutBook(Book book) {
+
+		if (book.getAvail()) {
+			book.checkout();
+			Calendar cal = Calendar.getInstance();
+			cal.add(Calendar.DATE, 10);
+
+			return cal.getTime();
+		}else {
+			throw new IllegalStateException();
+		}
 	}
 }
