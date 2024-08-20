3bf9e5741ab650ce6159964b76d2c6308d3e7f25
William Ngo <89047738+bigbillywilly@users.noreply.github.com>
2024-08-19 19:50:52 -0600

    Update Member.java
    Just some comments about making this class generic so we can just have one check out method

--- Member.java
+++ Member.java
@@ -7,7 +7,8 @@ public class Member extends User {
 	public Member(int id, String name, String username, String email) {
 		super(name, username, email);
 	}
-	
+
+	//we should make this a generic class with generic methods so we can use checkOut on books albums and anything else we want to add.
 	public Date checkOutBook(Book book) {
 
 		if (book.getAvail()) {
